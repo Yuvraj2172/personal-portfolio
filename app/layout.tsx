3d79bf7a8a46b6492b00cc52a918643971cd202a
Yuvraj Soni <demonsroar2172@gmail.com>
2024-02-02 03:02:23 +0530

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -21,8 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}</body>
+        <div className="flex flex-col items-center  " >
+
+        <Navbar />
+        {children}
+        </div>
+      </body>
     </html>
   );
 }
